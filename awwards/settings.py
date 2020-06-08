012919ac3b30e7d2d19b6747c47d345bb407e266
moselamar <44118179+moselamar@users.noreply.github.com>
2020-06-08 17:51:27 +0300

    added testframework app

--- awwards/settings.py
+++ awwards/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awards',
+    'bootstrap3',
+    'bootstrap4',
+    'tinymce',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
