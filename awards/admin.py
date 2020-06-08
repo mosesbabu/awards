b4dd8bbc74f7342c065a9de9b97180460f7ff34c
moselamar <44118179+moselamar@users.noreply.github.com>
2020-06-08 16:18:23 +0300

    egister models to admin

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import categories,technologies,colors,countries,Project,Profile,Rating
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    filter_horizontal =('technologies','categories','colors')
+
 
 # Register your models here.
+admin.site.register(categories)
+admin.site.register(technologies)
+admin.site.register(colors)
+admin.site.register(countries)
+admin.site.register(Project,ProjectAdmin)
+admin.site.register(Profile)
+admin.site.register(Rating)
